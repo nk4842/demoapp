b62ed9ce8c0be81ec0d8735e3b360de4fc4d32f2
nk4842 <orbit@fermid-no-MacBook-Air.local>
2013-01-02 23:04:47 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoapp::Application.config.secret_token = '0ec7e9d914fd832c49227dd800e4b39ee27779bc13ae20534769503eb6b43663a800dc9ff0a8b94f1e8002c1cd9d04b6e91cbc50926fb4fb5f4efaa7b9abb8ee'
